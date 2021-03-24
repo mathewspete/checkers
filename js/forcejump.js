eff0ae28e86eff8ad0609ba2306c1fc7341282cf
Pete Mathews <petemathews14@gmail.com>
2021-03-24 07:53:25 -0400

    selection and idea of needs

--- js/forcejump.js
+++ js/forcejump.js
@@ -1,6 +1,51 @@
 var turnCt = 0;
 var turn;
+var color;
 var activeSelect;
+var teamRed = 12;
+var teamBlack = 12;
+var fromTo = [];
+
+/*
+
+while (teamRed>0 && teamBlack>0)
+    // no must jump, but must complete double jumps.
+        click a square
+        check if square has a piece
+        if it the piece is the color for turn, set turnToggle to TRUE
+        toggle an "active" class to highlight the selected piece
+        wait the next click
+            if click has checker same as turn, set that square to active
+            if click = +/- 1 rc && square is open, move active piece.
+            if click = +/- 2 rc && rc +/- 1 is opp color, clear the opp piece teamOpp-- and move active piece.
+                if +/- 2 is open and +/- 1 is opp color, keep square active.
+                else, end turn
+*/
+
+
+function active(params) {
+    var square = [];
+    $("div").removeClass("active");
+    var player = (turn === 'black') ? 'red' : 'black';
+    if ($(this).data('status') === turn) {
+        square.push($(this).data('row'));
+        square.push($(this).data('col'));
+        fromTo[0] = square;
+        var activeSelect = $(this).find('div').attr('id');
+        $(this).toggleClass('active');
+        console.log(fromTo);
+    } if ($(this).data('status') === player) {
+        alert(`Nice try!\n. . .But it's not ${player}'s turn!`)
+    } else { }
+}
+
+
+
+
+
+
+
+
 
 console.log('hello');
 
@@ -9,9 +54,6 @@ function getLocale() {
     console.log(`${$val.data('row')}, ${$val.data('col')}`);
 }
 
-function active(params) {
-    var selection = $(this).find('div').attr('id');
-}
 
 
 function isQueen() {
@@ -31,7 +73,7 @@ function jump() {
 }
 
 function makeMove() {
-
+    turnCt++;
 }
 
 function isTurn() {
@@ -39,13 +81,17 @@ function isTurn() {
 }
 function getColor(sqr) {
     var alt = $(this).find('div').attr('class');
-    turnCt++;
+    //console.log(alt.includes('black'));
+    color = alt;
     alert(`inner div class is ${alt}, and turn #${turnCt}`);
     //let $color = this.find(".checker").data('player');
     //alert(`player ${$color}`);
 }
 
-//$(".square").on('click', fu);
+/*
+$(".square").on('click', fu);
 $('.square').click(getColor);//, getColor);
-$('.square').click(getLocale);
-
+*/
+$('.dark').click(isTurn);
+//$('div').click($("div").removeClass("active"));
+$('.dark').click(active);
