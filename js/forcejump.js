7e789619aef86f8ce1f06f63f8da24b1f93fb9f6
mathews225 <petemathews14@gmail.com>
2021-03-26 06:54:00 -0400

    single move

--- js/forcejump.js
+++ js/forcejump.js
@@ -8,7 +8,7 @@ status,
 color,
 turn,
 activeSelect;
-
+let debuggy = {};
 
 /*
 
@@ -30,52 +30,116 @@ function isTurn(){
     turn = (turnCt % 2 === 0) ? "black" : 'red';
 }
 
-function active(params) {
-    let square = [];
-    let status = $(this).data('status')
+function active() {
+    status = "";
+    status = $(this).attr('data-status');
+    console.log(`status = ${status}`);
 
     $("div").removeClass("active");
     player = (turn === 'black') ? 'red' : 'black';
 
     switch (status) {
         case turn:
+            debuggy.trigger = "status.player"
+            debuggy.status = status;
+            debuggy.turn = turn;
+            debuggy.turnToggle = turnToggle;
+            debuggy.coords = coords;
+            debuggy.player = player;
+            debuggy.class = $(this).attr("class");
+            debuggy.id = activeSelect;
+            console.log(debuggy);
             turnToggle = true;
-            coords.row = ($(this).data('row'));
-            coords.column = ($(this).data('col'));
-            var activeSelect = $(this).find('div').attr('id');
+            coords.row = ($(this).attr('data-row'));
+            coords.column = ($(this).attr('data-col'));
+            activeSelect = $(this).find('div').attr('id');
             $(this).toggleClass('active');
-            console.log(coords);
+            console.log(`coords = ${coords}`);
             break;
         case player:
-            alert(`Nice try!\n. . .But it's not ${player}'s turn!`);
+            debuggy.trigger = "status.player"
+            debuggy.status = status;
+            debuggy.turn = turn;
+            debuggy.turnToggle = turnToggle;
+            debuggy.coords = coords;
+            debuggy.player = player;
+            debuggy.class = $(this).attr("class");
+            debuggy.id = activeSelect;
+            console.log(debuggy);
+            console(`Nice try!\n. . .But it's not ${player}'s turn!`);
             turnToggle = false;
             break;
 
         default:
-            console.log('default');
-            let rOffset = $(this).data('row') - coords.row;
-            let cOffset = $(this).data('col') - coords.column;
-            console.log('rOffset ' + rOffset + ' | cOffset ' + cOffset);
+            debuggy.trigger = "status.default"
+            debuggy.status = status;
+            debuggy.turn = turn;
+            debuggy.turnToggle = turnToggle;
+            debuggy.coords = coords;
+            debuggy.player = player;
+            debuggy.class = $(this).attr("class");
+            debuggy.id = activeSelect;
+            console.log(debuggy);
+            let rOffset = $(this).attr('data-row') - coords.row;
+            let cOffset = $(this).attr('data-col') - coords.column;
+            columnOffsetNotJump = (cOffset === -1 || cOffset === 1);
+            let jumpCol = coords.column+(cOffset/2);
+            let jumpee = document.getElementById(`${coords.row-1}-${jumpCol}`);
+
+
             switch (turn) {
                 case "black":
                     switch (rOffset) {
                         case -1:
-                            if (cOffset === -1 || cOffset === 1) {
-                                makeMove($(this).data('row'), $(this).data('col'))
+                            if (columnOffsetNotJump) {
+                                makeMove($(this).attr('data-row'), $(this).attr('data-col'))
+                            }
+                            break;
+                        case -2:
+                            if (checkJump(cOffset)) {
+                                makeMove($(this).attr('data-row'), $(this).attr('data-col'));
                             }
                             break;
 
                         default:
                             break;
                     }
+                    break;
+
+                case "red":
+                    switch (rOffset) {
+                        case 1:
+                            if (columnOffsetNotJump) {
+                                makeMove($(this).attr('data-row'), $(this).attr('data-col'))
+                            }
+                            break;
+                        case 2:
+                            if (checkJump(cOffset)) {
+                                makeMove($(this).attr('data-row'), $(this).attr('data-col'));
+                            }
+                            break;
+
+                        default:
+                            break;
+                    }
+                    break;
+                default:
+                    break;
             }
             break;
     }
 }
 
+const checkJump = (column) => {
+    let jumpCol = parseInt(coords.column) + (column/2);
+    console.log(`${coords.row-1}-${coords.column}-${column}`);
+    let jumpee = document.getElementById(`${coords.row-1}-${jumpCol}`);
+    console.log(`${coords.row-1}-${jumpCol}`);
+    let jumpeeId = jumpee.getAttribute('data-status');
+    return (jumpeeId === player && (column === -2 || column === 2));
+}
 
-
-function makeMove(row, column) {
+const makeMove = (row, column) => {
     // can add some animation here at some point.
     let moveToId = document.getElementById(`${row}-${column}`);
     let moveFromId = document.getElementById(`${coords.row}-${coords.column}`);
@@ -83,10 +147,11 @@ function makeMove(row, column) {
     moveFromId.innerHTML = "";
     moveToId.innerHTML = piece;
     turnToggle = false;
-}
-
-const makeMove = (selectedRow, selectedCol) => {
-    console.log(`sqr = ${selectedRow}, ${selectedCol}`);
+    turnCt++ // will delete
+    moveFromId.setAttribute('data-status', "open");
+    moveToId.setAttribute('data-status', turn);
+    coords = {};
+    activeSelect = "";
 }
 
 
@@ -96,13 +161,13 @@ console.log('hello');
 
 function getLocale() {
     let $val = $(this);
-    console.log(`${$val.data('row')}, ${$val.data('col')}`);
+    console.log(`${$val.attr('data-row')}, ${$val.attr('data-col')}`);
 }
 
 
 
 function isQueen() {
-    if ($(this).data('queen') === true) {
+    if ($(this).attr('data-queen') === true) {
         return true;
     } else {
         return false;
@@ -113,17 +178,13 @@ function makeQueen(id) {
 
 }
 
-function jump() {
-
-}
-
 
 function getColor(sqr) {
     var alt = $(this).find('div').attr('class');
     //console.log(alt.includes('black'));
     color = alt;
     alert(`inner div class is ${alt}, and turn #${turnCt}`);
-    //let $color = this.find(".checker").data('player');
+    //let $color = this.find(".checker").attr('data-player');
     //alert(`player ${$color}`);
 }
 
@@ -133,4 +194,8 @@ $('.square').click(getColor);//, getColor);
 */
 $('.dark').click(isTurn);
 //$('div').click($("div").removeClass("active"));
-$('.dark').click(active);
+$('.dark').on("click", active);
+
+const click = (id) => {
+
+}
